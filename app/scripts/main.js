2a01caadeedb3868c1df61443873abda670bb406
marras0914 <arras.marco@gmail.com>
2019-03-14 16:50:01 -0500

    adding form validation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,3 +7,5 @@ console.log('\'Allo \'Allo!');
 // Uncomment to enable Bootstrap popovers
 // https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
 // $(function () { $('[data-toggle="popover"]').popover(); });
+
+$("#form").validate();
